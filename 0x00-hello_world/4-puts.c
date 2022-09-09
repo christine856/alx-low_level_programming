6d5a68e1f9590d076de8e5ba28337480b15eb241
christine856 <mattichristine@yahoo.com>
2022-09-08 17:41:36 -0700

    going

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,20 @@
 
 
 
-/**
+its successful)/**
  *
  * * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- * * Return: 0 if exited properly, non-zero otherwise
+ * * Return: 0 
  *
- * */
+ * /
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+return (0);
 
 }
