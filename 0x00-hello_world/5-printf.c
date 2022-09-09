dfad7edc15555c710ba0b698ae5ad4a2a1d1cd44
christine856 <mattichristine@yahoo.com>
2022-09-08 19:01:45 -0700

    cut

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 
 
@@ -16,5 +16,5 @@ include <stdio.h>
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+return (0);                           
 }
