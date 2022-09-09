78e0977b3396997bbdd24a60d0e47e9cf27e77c0
christine856 <mattichristine@yahoo.com>
2022-09-08 17:48:47 -0700

    getting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,15 @@
 
 
 
-its successful)/**
+/**
  *
  * * main - prints the phrase "Programming is like building a multilingual puzzle
  *
  * * Return: 0 
  *
  * /
-
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
