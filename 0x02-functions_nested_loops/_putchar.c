495ae17590c1bf735925829c82f415a23df63802
christine856 <mattichristine@yahoo.com>
2022-09-14 15:41:45 -0700

    loyal

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**main - prints _putchar, followed by a new line.
+ * Return: 0 when successful
+ * Descripton - The program should return 0
+ */
+
+int main (void)
+{
+char s[] = "_putchar";
+int i = 0;
+while (s[] = != '\0')
+{
+_putchar(s[i]);
+i++;
+}
+_putchar(10);
+return (0);
+}
