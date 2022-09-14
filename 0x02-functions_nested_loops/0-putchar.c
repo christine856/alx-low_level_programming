a651d7c045cf5dc7543d8a5aa65c22b6c727cc48
christine856 <mattichristine@yahoo.com>
2022-09-14 14:26:47 -0700

    forever

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"                                                                                                                        
 
-/**                                                                                                                                   
- *main - prints _putchar, followed by a new line.                                                                                       
- *Return: 0 when successful                                                                                                             
- *Description - The program should return 0                                                                                             
- */                                                                                                                                    
-int main(void)                                                                                                                       
-{                                                                                                                                        
-_putchar('_');                                                                                                                   
-_putchar('p');                                                                                                                   
-_putchar('u');                                                                                                                   
-_putchar('t');                                                                                                                   
-_putchar('c');                                                                                                                   
-_putchar('h');                                                                                                                   
-_putchar('a');                                                                                                                   
-_putchar('r');                                                                                                                   				
+/*
+ *main - prints _putchar, followed by a new line.                                
+ *Description - The program should return 0
+ *Return: 0 when successful                                      
+ */                                                                              
+int main(void)                                                                  
+{                                                                              
+_putchar('_');                                                                  
+_putchar('p');                                                                  
+_putchar('u');                                                                  
+_putchar('t');                                                                  
+_putchar('c');                                                                  
+_putchar('h');                                                                  
+_putchar('a');                                                                  
+_putchar('r');                                                                 
 _putchar('\n');
-
 return (0);                                                                                                                      
 }
