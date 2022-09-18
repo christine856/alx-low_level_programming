aa49b79338c578dca5a12308c1a53ec2de9e7670
christine856 <mattichristine@yahoo.com>
2022-09-18 16:57:02 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 *print_most_numbers - prints the numbers, from 0 to 9,
 *except 2 and 4, followed by a new line
@@ -16,5 +15,6 @@ if (i != 2 && i != 4)
 _putchar(i + '0');
 }
 }
+
 _putchar('\n');
 }
